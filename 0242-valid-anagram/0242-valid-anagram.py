910395a1b237f3fc4930a02bc002bfec30ea8ca6
Pratik Haladkar <pratikhaladkar01@gmail.com>
2026-08-14 16:51:48 +0530

    Time: 11 ms (77.35%), Space: 19.5 MB (46.22%) - LeetHub - Time: O(n), Space: O(n)

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,25 +1,23 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        dict_t = {}
-        for strs in t:
-            if strs in dict_t :
-                dict_t[strs] += 1
-            else:
-                dict_t[strs] = 1
-        
-        dict_s = {}
-        for strs in s:
-            if strs in dict_s :
-                dict_s[strs] += 1
-            else:
-                dict_s[strs] = 1
-
-        if dict_s == dict_t:
-            return True
-        else:
+        if len(s) != len(t):
             return False
 
-        
+        count = {}
+
+        for char in s:
+            count[char] = count.get(char, 0) + 1
+
+        for char in t:
+            if char not in count:
+                return False
+
+            count[char] -= 1
+
+            if count[char] < 0:
+                return False
+
+        return True
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
